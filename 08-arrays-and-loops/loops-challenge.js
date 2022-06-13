6aef2d98119039e7811fb3075e353d197dcf12e6
Ben Davis <ben@boldidea.org>
2022-06-13 19:47:41 +0000

    Grammar, typo, and other fixes (thanks to Spencer Baumruk!)

--- 08-arrays-and-loops/loops-challenge.js
+++ 08-arrays-and-loops/loops-challenge.js
@@ -1,14 +1,14 @@
 
 /**
  * 1. Use a counting loop to output the square of every
- *    number from 0 to 10.
+ *    number from 0 to 9.
  */
 
 // --- loop goes here ---
 
 /**
  * 2. Use a for...of loop to output "Have a ______ day"
- *    for each of the acjectives (using console.log)
+ *    for each of the adjectives (using console.log)
  */
 
 let adjectives = ["fantastic", "great", "super", "fun"];
